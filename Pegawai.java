22022318cb7ef16718da526b549761f2e815eaa7
riskianaxagoras <sasukekakashi69sharingan@gmail.com>
2025-11-06 19:42:18 +0700

    Add files via upload

---
+++ Pegawai.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pegawai;
+
+/**
+ *
+ * @author Mirzaa
+ */
+public class Pegawai {
+    public String nama;
+    public int gajiPokok;
+    
+    public void tampilData() {
+        System.out.println("NAMA = " + nama);
+        System.out.println("GAJI POKOK" + gajiPokok);
+    }
+}
